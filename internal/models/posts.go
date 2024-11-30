e7f5f2d205987428fda913c63f9f706bc0fd43c7
NinaSayers <massypaz2001git@gmail.com>
2024-11-30 11:45:44 -0500

    add: Create Post in Models

--- internal/models/posts.go
+++ internal/models/posts.go
@@ -3,14 +3,15 @@ package models
 import (
 	"database/sql"
 	"time"
+	"fmt"
 )
 
 type Post struct {
-	post_id	   int
-	user_id    int
-	content    string
-	created_at time.Time
-	updated_at time.Time
+	PostID	   int		`json:"post_id"`
+	UserID     int		`json:"user_id"`
+	Content    string	`json:"content"`
+	CreatedAt time.Time	`json:"created_at"`
+	UpdatedAT time.Time	`json:"updated_at"`
 }
 
 type PostModel struct {
@@ -18,8 +19,31 @@ type PostModel struct {
 }
 
 func (m *PostModel) Create(user_id int, content string) (int, error){
-	return 0, nil
+
+	var count int
+	err := m.DB.QueryRow("SELECT COUNT(*) FROM users WHERE id = ?", user_id).Scan(&count)
+	if err != nil {
+	 return 0, fmt.Errorf("error checking user existence: %w", err)
+	}
+	if count == 0 {
+	 return 0, fmt.Errorf("user with ID %d does not exist", user_id)
+	}
+	
+	stmt := `INSERT INTO posts (user_id, content) VALUES (?, ?)`
+	
+	result, err := m.DB.Exec(stmt, user_id, content)
+	if err != nil {
+	 return 0, fmt.Errorf("error creating post: %w", err)
+	}
+	
+	id, err := result.LastInsertId()
+	if err != nil {
+	 return 0, fmt.Errorf("error getting post ID: %w", err)
+	}
+	
+	return int(id), nil
 }
+
 
 func (m *PostModel) Get(id int) (*Post, error){
 	return nil, nil
